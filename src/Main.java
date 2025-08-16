9fc94c51b362f1573f000f1ac1866c2a5f021a0d
Romulo <romuabm444@gmail.com>
2025-08-16 14:18:22 -0300

    fatorial

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int resultado = 0;
-        int fatorial = 0;
+        int numeroInformado = 0;
         System.out.println("Digite um número e eu irei mostrar o fatorial desse número: ");
-        fatorial = scanner.nextInt();
-        int multiplicador = fatorial;
-        while (multiplicador > 0) {
-            resultado = fatorial * (multiplicador - 1);
-            multiplicador--;
-            System.out.println(resultado);
+        numeroInformado = scanner.nextInt();
+        int num = numeroInformado -1;
+        numeroInformado = numeroInformado * (numeroInformado-1);
+        System.out.println(numeroInformado);
+        while( num != 1){
+            numeroInformado = numeroInformado * (num-1);
+            num--;
+
+            System.out.println(numeroInformado);
         }
     }
 }
