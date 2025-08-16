94926930289a5206890c87d6e30c0a0c1c3d9940
Romulo <romuabm444@gmail.com>
2025-08-16 14:03:33 -0300

    fatorial

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int resultado = 0;
+        int fatorial = 0;
+        System.out.println("Digite um número e eu irei mostrar o fatorial desse número: ");
+        fatorial = scanner.nextInt();
+        int multiplicador = fatorial;
+        while (multiplicador > 0) {
+            resultado = fatorial * (multiplicador - 1);
+            multiplicador--;
+            System.out.println(resultado);
         }
     }
 }
